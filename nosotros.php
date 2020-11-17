51ccc4a9ec5618555190478ad3b931bcfdd20cf3
fervegag <fervega.ipn@gmail.com>
2020-11-17 12:41:25 -0600

    Header y rutas

--- nosotros.php
+++ nosotros.php
@@ -22,7 +22,7 @@
             <nav class="navigation">
                 <ul class="show">
                     <li><a href="index.php">Inicio</a></li>
-                    <li><a href="#">Nosotros</a></li>
+                    <li><a href="nosotros.php">Nosotros</a></li>
                     <li><a href="#">Deporte</a></li>
                     <li><a href="contacto.php">Contacto</a></li>
                     <li><a href="login.php">Profesores</a></li>
